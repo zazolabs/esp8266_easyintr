882e0e113f1295a0cce2856e59faf071e7d922a2
Alexander Atanasov <alex@zazolabs.com>
2015-02-16 14:45:58 +0200

    add define to force isr into .iram1.text section

--- easyintr.h
+++ easyintr.h
@@ -8,5 +8,6 @@ bool ei_detach(uint8_t gpio_pin);
 /* Install GPIO interrupt handler */
 void ei_attach(uint8 gpio_pin, uint32 gpio_name, uint8 gpio_func, void (*vect)(void *), void *data);
 
+#define ISR_HANDLER __attribute__((section(".iram1.text")))
 
 #endif
